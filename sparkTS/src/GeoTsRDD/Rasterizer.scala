58bccda0c9ca2a4b36b93d2a0c34bceb23bc647f
bellettif <bellettif@gmail.com>
2015-06-22 00:10:46 -0700

    Solved bugged and tested with python notebook on NY data.

--- sparkTS/src/GeoTsRDD/Rasterizer.scala
+++ sparkTS/src/GeoTsRDD/Rasterizer.scala
@@ -3,6 +3,7 @@ package GeoTsRDD
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext._
+import org.apache.spark.sql._
 
 import scala.reflect.ClassTag
 
@@ -14,13 +15,13 @@ import scala.reflect.ClassTag
 TODO: Internal RDD as IndexedRDD (mesh index for spatial data)
 TODO: Convert to DataFrame representation
  */
-case class Rasterizer[LocType, AttribType: ClassTag, MergeType](
-  private val grid: LocType => Long,
+case class Rasterizer[LocType, HashedType: ClassTag, AttribType: ClassTag, MergeType](
+  private val grid: LocType => HashedType,
   private val aggregator: Aggregator[AttribType, MergeType],
-  private val postMapper: ((Long, MergeType)) => (Long, AttribType),
+  private val postMapper: ((HashedType, MergeType)) => (HashedType, AttribType),
   private val rawData: RDD[LocEvent[LocType, AttribType]]) {
 
-  private val rasters: RDD[(Long, AttribType)] = rawData
+  val rasters: RDD[(HashedType, AttribType)] = rawData
     .map(x =>(grid(x.getLoc()), x.getAttrib()))
     .combineByKey(aggregator.initializer, aggregator.valueMerger, aggregator.valueCombiner)
     .map(postMapper)
